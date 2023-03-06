40e967a95c99d480b82c13deaf3aee70f86840ce
polakimahesh <94633440+polakimahesh@users.noreply.github.com>
2023-03-06 21:13:02 +0530

    Delete src directory

--- src/main/java/com/javaproject/Ecommerce/Products/ProductRepository.java
+++
@@ -1,6 +0,0 @@
-package com.javaproject.Ecommerce.Products;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface ProductRepository extends JpaRepository<Product,Integer> {
-
-}
